886231f198c8dd494b39158d107146b63326cd0b
linhan <linhan@localhost.localdomain>
2020-08-01 21:11:37 +0800

    add snsprintf benchmark

--- json/main.cpp
+++ json/main.cpp
@@ -77,6 +77,15 @@ struct Pod {
 
         json.dump();
     }
+
+    void Snsprintf() {
+        char buffer[1024];
+
+        int cx = snprintf(buffer, 1024, "{\"field1\":%d,\"field2\":%s,\"field3\":%lf,\"field4\":%zd,"
+                                        "\"field5\":%lf,\"field6\":%zd,\"field7\":%zd,\"field8\":%zd,\"field9\":%zd,"
+                                        "\"field10\":%zd,}", field1, field2, field3, field4, field5,
+                                        field6, field7, field8, field9, field10);
+    }
 };
 
 static void BM_RAPIDJSON_SERIALIZE(benchmark::State& state) {
@@ -101,8 +110,19 @@ static void BM_NLOHMANNJSON_SERIALIZE(benchmark::State& state) {
     }
 }
 
+static void BM_SNSPRINTF(benchmark::State& state) {
+    Pod pod{.field1 = 34123, .field2 = "wanglinhan", .field3 = 1243.121, .field4 = 23433, .field5 = 234.234,
+            .field6 = 23424, .field7 = 23424, .field8 = 23424, .field9 = 23424, .field10 = 23424,};
+
+    for (auto _ : state) {
+        pod.Snsprintf();
+    }
+}
+
 // Register the function as a benchmark
 BENCHMARK(BM_RAPIDJSON_SERIALIZE);
 BENCHMARK(BM_NLOHMANNJSON_SERIALIZE);
+BENCHMARK(BM_SNSPRINTF);
+
 // Run the benchmark
 BENCHMARK_MAIN();
