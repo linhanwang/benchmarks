83b684a3a00ee622161da3cedde029a8d5c11868
wanglinhan <wanglinhan.pku@gmail.com>
2020-12-06 18:21:47 +0800

    adjust sum benchmark

--- simd_sum/simd_sum.benchmark.cpp
+++ simd_sum/simd_sum.benchmark.cpp
@@ -1,5 +1,6 @@
 #include <benchmark/benchmark.h>
 #include <blaze/Blaze.h>
+#include <functional>
 
 #include <iostream>
 
@@ -11,35 +12,40 @@ static void BM_LoopSum20Double(benchmark::State& state) {
         arr[i] = i * 2.0;
     }
 
-    double sum = 0;
     for (auto _ : state) {
-        sum = 0;
+        double sum = 0;
         for (int i = 0; i < kArrSize; ++i) {
             sum += arr[i];
         }
         benchmark::DoNotOptimize(sum);
     }
-    std::cout << sum << std::endl;
+}
+
+static void BM_AccumulateSumDouble(benchmark::State& state) {
+    std::vector<double> arr(kArrSize);
+    for (int i = 0; i < kArrSize; ++i) {
+        arr[i] = i * 2.0;
+    }
+
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(std::accumulate(arr.begin(), arr.end(), 0));
+    }
 }
 
 static void BM_DotProductSum20Double(benchmark::State& state) {
     blaze::StaticVector<double, kArrSize> arr;
-    blaze::StaticVector<double, kArrSize> one_arr;
     for (int i = 0; i < kArrSize; ++i) {
-        one_arr[i] = 1;
         arr[i] = i * 2.0;
     }
 
-    double sum = 0;
     for (auto _ : state) {
-        sum = blaze::trans(one_arr) * arr;
-        benchmark::DoNotOptimize(sum);
+        benchmark::DoNotOptimize(blaze::sum(arr));
     }
-    std::cout << sum << std::endl;
 }
 
 // Register the function as a benchmark
 BENCHMARK(BM_LoopSum20Double);
+BENCHMARK(BM_AccumulateSumDouble);
 BENCHMARK(BM_DotProductSum20Double);
 
 // Run the benchmark
