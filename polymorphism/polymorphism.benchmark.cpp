756489dc806596e8b6b0e933873d279302c8e951
wanglinhan <wanglinhan.pku@gmail.com>
2021-05-16 22:20:51 +0800

    refactor polymohpism benchmark

--- polymorphism/polymorphism.benchmark.cpp
+++ polymorphism/polymorphism.benchmark.cpp
@@ -1,80 +1,124 @@
 #include <benchmark/benchmark.h>
 
-#include <vector>
+#include <memory>
+#include <functional>
 
 struct Base {
-  virtual void Add() = 0;
+  virtual int& Add() = 0;
 };
 
 struct Derived : public Base {
   int data{0};
 
-  void Add() override { ++data; }
+  int& Add() override { return ++data; }
 };
 
 struct Derived1 : public Base {
   int data{0};
 
-  void Add() override { data += 2; }
+  int& Add() override { return data += 2; }
 };
 
 struct Derived2 : public Base {
   int data{0};
 
-  void Add() override { data += 3; }
+  int& Add() override { return data += 3; }
 };
 
 struct Derived3 : public Base {
   int data{0};
 
-  void Add() override { data += 4; }
+  int& Add() override { return data += 4; }
 };
 
-void DynamicPolymorphicTranverse(std::vector<Base*>& vec) {
-  for (auto& item : vec) {
-    item->Add();
-  }
-}
-
-template <typename T>
-void StaticPolymorphicTranverse(std::vector<T*>& vec) {
-  for (auto& item : vec) {
-    item->Add();
-  }
-}
-
 static void BM_DynamicPolymorphism(benchmark::State& state) {
-  std::vector<Base*> base_vec;
+  Derived1 derived1{};
+  Derived3 derived3{};
+  Base* base{nullptr};
 
-  Derived2 derived;
-
-  for (int i = 0; i < 100; ++i) {
-    base_vec.emplace_back(&derived);
-  }
-
+  benchmark::DoNotOptimize(base = &derived3);
   for (auto _ : state) {
-    benchmark::DoNotOptimize(derived.data);
-    DynamicPolymorphicTranverse(base_vec);
+    for (int i = 0; i < 1024; ++i) {
+      benchmark::DoNotOptimize(base->Add());
+    }
   }
 }
+
+
+struct StaticDerived {
+  int data{0};
+
+  int& Add() { return ++data; }
+};
+
+struct StaticDerived1 {
+  int data{0};
+
+  int& Add() { return data += 2; }
+};
+
+struct StaticDerived2 {
+  int data{0};
+
+  int& Add() { return data += 3; }
+};
+
+struct StaticDerived3 {
+  int data{0};
+
+  int& Add() { return data += 4; }
+};
+
+struct ILogicManager {
+  virtual void MainLoop() = 0;
+  virtual ~ILogicManager() = default;
+};
+
+template <class T>
+struct LogicManager : public ILogicManager {
+  void MainLoop() final {
+    for (int i = 0; i < 1024; ++i) {
+      benchmark::DoNotOptimize(adder.Add());
+    }
+  }
+
+  T adder{};
+};
+
+std::unique_ptr<ILogicManager> Factory(int router) {
+  switch (router) {
+    case 0:
+      return std::make_unique<LogicManager<StaticDerived>>();
+    case 1:
+      return std::make_unique<LogicManager<StaticDerived1>>();
+    case 2:
+      return std::make_unique<LogicManager<StaticDerived2>>();
+    default:
+      return std::make_unique<LogicManager<StaticDerived3>>();
+  }
+}
+
 
 static void BM_StaticPolymorphism(benchmark::State& state) {
-  std::vector<Derived2*> vec;
-
-  Derived2 derived;
-
-  for (int i = 0; i < 100; ++i) {
-    vec.emplace_back(&derived);
+  auto logic = Factory(3);
+  for (auto _ : state) {
+    logic->MainLoop();
   }
+}
+
+static void BM_StaticPolymorphism2(benchmark::State& state) {
+  StaticDerived3 derived{};
 
   for (auto _ : state) {
-    benchmark::DoNotOptimize(derived.data);
-    StaticPolymorphicTranverse(vec);
+    for (int i = 0; i < 1024; ++i) {
+      benchmark::DoNotOptimize(derived.Add());
+    }
   }
 }
 
 // Register the function as a benchmark
 BENCHMARK(BM_DynamicPolymorphism);
 BENCHMARK(BM_StaticPolymorphism);
+BENCHMARK(BM_StaticPolymorphism2);
 
 // Run the benchmark
